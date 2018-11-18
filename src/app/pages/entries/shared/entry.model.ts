1500a214e529352359b9acc3777267f79e11d2d9
danieldelajustina <dd.justina@hotmail.com>
2018-11-18 14:58:47 -0200

    entry module

---
+++ src/app/pages/entries/shared/entry.model.ts
@@ -0,0 +1,24 @@
+import { Category } from '../../categories/shared/category.model';
+
+export class Entry {
+    constructor(
+        public id?: number,
+        public name?: String,
+        public description?: String,
+        public type?: String,
+        public amount?: String,
+        public date?: String,
+        public paid?: boolean,
+        public categoryId?: number,
+        public category?: Category
+    ) {}
+
+    static types = {
+        expense: 'Desepesa',
+        renevue: 'Receita'
+    };
+
+    get paidText(): String {
+        return this.paid ? 'Pago' : 'Pendente';
+    }
+}
