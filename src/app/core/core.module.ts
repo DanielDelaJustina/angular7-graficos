ffe3e3513052f84f6971f0e4225916bb399921ac
danieldelajustina <dd.justina@hotmail.com>
2018-12-18 19:59:48 -0200

    add navBarComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,24 +4,32 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataBase } from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase),
+    RouterModule
   ],
   exports: [
     // shared module
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+
+    // shared components
+    NavbarComponent
   ]
 })
 export class CoreModule { }
